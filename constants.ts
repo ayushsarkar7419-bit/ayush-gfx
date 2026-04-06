914649ee442fba6ab509cfd5f5a32bbd48992f2c
ayushsarkar7419-bit <ayushsarkar7419@gmail.com>
2026-04-07 00:47:36 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -95,18 +95,53 @@ export const THUMBNAILS: ThumbnailItem[] = [
     imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459158/techno_thumbnail_bkpe24.jpg',
     ctr: '10.9% CTR'
   },
-    { 
+  { 
     id: 'new-4', 
     title: '2k Per Clients', 
     category: 'Finance', 
     imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459293/How_to_Sign_High_xmgoj4.jpg',
     ctr: '8.9% CTR'
   },
-    { 
+  { 
     id: 'new-4', 
     title: 'Rocket league', 
     category: 'Gaming', 
     imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459098/rocket_leauge_fbmnxc.jpg',
+    ctr: '10.9% CTR'
+  },
+  { 
+    id: 'new-4', 
+    title: 'gay or straight', 
+    category: 'IRL', 
+    imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459155/gay_or_straight_cmcbs8.jpg',
+    ctr: '10.9% CTR'
+  },
+  { 
+    id: 'new-4', 
+    title: 'squid Game', 
+    category: 'IRL', 
+    imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459157/squid_game.jpg_1_dwa528.jpg',
+    ctr: '10.9% CTR'
+  },
+  { 
+    id: 'new-4', 
+    title: 'Editing is so boring', 
+    category: 'TECH', 
+    imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459375/Editing_is_so_boring.jpg_2_g5pqgw.jpg',
+    ctr: '10.9% CTR'
+  },
+  { 
+    id: 'new-4', 
+    title: 'JJK', 
+    category: 'Gaming', 
+    imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459098/jjk_ak9emy.jpg',
+    ctr: '10.9% CTR'
+  },
+  { 
+    id: 'new-4', 
+    title: 'Open ai Kill', 
+    category: 'Tech', 
+    imageUrl: 'https://res.cloudinary.com/deav91wr9/image/upload/v1775459331/OpenAI_Just_Killed_Their_AI_Companion_Model_byrlmg.jpg',
     ctr: '10.9% CTR'
   },
   // 11 New Gaming Exclusive Thumbnails (Hidden from All)
